b77a1af372e28babc5fcb93f5927fe5bc475b411
zeckart <ferrari.robin@gmail.com>
2018-05-01 14:51:41 +0200

    Ajout d'une page exemple

---
+++ views/exemple.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="container">
+	Ceci est une page d'exemple affichée depuis le modèle<br>
+	Elle peut également récupérer des variable depuis son model (/models/exemple.php)<br>
+	
+	Valeur de la variable $test définie dans le modèle est <?php echo $test; ?>
+</div>
+<?php get_footer(); ?>
